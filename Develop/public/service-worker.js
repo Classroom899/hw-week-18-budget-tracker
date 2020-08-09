4f53336fc5de41034d0851e03dbde54a8647770f
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-09 12:31:21 -0700

    modified service-worker.js fetch

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -35,14 +35,36 @@ self.addEventListener("activate", function(evt) { // Adding an event listener fo
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) { // Adding an event listener for fetch - This will intercept any fetch requests to the back end 
-evt.respondWith(
-  caches.open(CACHE_NAME).then((cache) => {
-  return cache.match(evt.request)  
+self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              console.log(response)
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return; 
   }
-  )
-)
-});
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request); // To keep going with this call and find it
+    })
+  );})
+
 
 // self.addEventListener("fetch", function(evt) { // Adding an event listener for fetch - This will intercept any fetch requests to the back end 
 //   if (evt.request.url.includes("/api/")) {

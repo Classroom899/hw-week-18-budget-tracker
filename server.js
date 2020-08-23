b830da920af0f08d167f809fbe58f80c8946d808
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-23 08:33:21 -0700

    changed database connectivity in server.js

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", {
+  useNewUrlParser: true
+}).then(data => console.log("Data")).catch(error => console.log("Hello World"));
 
 // routes
 app.use(require("./routes/api.js"));

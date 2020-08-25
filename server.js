580f81c327c8abae14f2868855f764029c4b0031
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-24 18:24:20 -0700

    changing the mongoose.connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/workout", {
   useNewUrlParser: true
 }).then(data => console.log("Data")).catch(error => console.log("Hello World"));
 
